3d53f531a39f05290504117e55ad9055b09ab6ce
script-deployer <script-deployer@me.com>
2015-07-27 15:58:48 +0100

    working search and replace

--- app.py
+++ app.py
@@ -4,13 +4,29 @@
 {{ scaffold.name | capitalize }} Service
 """
 import stackhut
+import re
+from docx import Document
 
 class DefaultService:
     def __init__(self):
         pass
 
-    def add(self, x, y):
-        return x + y
+    def replaceText(self, documentUrl, textToReplace, replacementText):
+        in_file = stackhut.download_file(documentUrl)
+        out_file = "out_{}".format(in_file)
+
+        document = Document(in_file)
+        searchre = re.compile(textToReplace)
+
+        for paragraph in document.paragraphs:
+            paragraph_text = paragraph.text
+            if paragraph_text:
+                if searchre.search(paragraph_text):
+                    paragraph.text = re.sub(textToReplace, replacementText, paragraph_text)
+
+        document.save(out_file)
+        return stackhut.put_file(out_file)
+
 
 # export the services
 SERVICES = {"Default": DefaultService()}
